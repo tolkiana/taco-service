95f7b1ec99e2167cd43cdee2890a68a0226f57da
Adrian Pena <rastadrian@gmail.com>
2018-05-28 00:56:20 -0400

    Updates tests so they don't try to use a real ResourceManager with AWS

--- src/test/kotlin/com/tolkiana/taco/tacoservice/TacoServiceApplicationTests.kt
+++ src/test/kotlin/com/tolkiana/taco/tacoservice/TacoServiceApplicationTests.kt
@@ -1,16 +1,29 @@
 package com.tolkiana.taco.tacoservice
 
+import com.tolkiana.taco.tacoservice.resource.ResourceService
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Matchers
+import org.mockito.Mockito.`when`
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class TacoServiceApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @MockBean
+    lateinit var resourceService: ResourceService
 
+    @Before
+    fun setUp() {
+        `when`(resourceService.getProducts(Matchers.anyString())).thenReturn(emptyList())
+    }
+
+    @Test
+    fun contextLoads() {
+
+    }
 }
