66ed517d187098b314c9bf68201537cb59697c21
Adrian Pena <rastadrian@gmail.com>
2018-05-28 19:44:34 -0400

    Updates bucket name so its dynamic

--- src/main/kotlin/com/tolkiana/taco/tacoservice/resource/AwsResourceService.kt
+++ src/main/kotlin/com/tolkiana/taco/tacoservice/resource/AwsResourceService.kt
@@ -6,13 +6,15 @@ import com.amazonaws.services.s3.transfer.TransferManager
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.tolkiana.taco.tacoservice.dto.Product
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.io.File
 
 @Service
 class AwsResourceService(private val objectMapper: ObjectMapper, private val amazonS3: AmazonS3) : ResourceService {
 
-    private val bucket: String = "taco-app"
+    @Value("\${cloud.aws.bucket}")
+    private lateinit var bucket: String
 
     override fun uploadProducts(name: String, product: List<Product>) {
         val content = objectMapper.writeValueAsBytes(product)
