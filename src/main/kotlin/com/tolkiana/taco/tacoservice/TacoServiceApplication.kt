5367f2ae07a96d92c75d935daa9e002a38c44258
Adrian Pena <rastadrian@gmail.com>
2018-05-27 22:00:43 -0400

    Enables FeignClient registry in project

--- src/main/kotlin/com/tolkiana/taco/tacoservice/TacoServiceApplication.kt
+++ src/main/kotlin/com/tolkiana/taco/tacoservice/TacoServiceApplication.kt
@@ -2,7 +2,9 @@ package com.tolkiana.taco.tacoservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 open class TacoServiceApplication
 
