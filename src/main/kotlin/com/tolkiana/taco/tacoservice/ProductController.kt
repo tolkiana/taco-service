df78f937d9b166c242e8e8776e10ec91b748624d
Adrian Pena <rastadrian@gmail.com>
2018-05-27 21:05:27 -0400

    Updates arrayOf() declarations in ProductController to use simpler []

--- src/main/kotlin/com/tolkiana/taco/tacoservice/ProductController.kt
+++ src/main/kotlin/com/tolkiana/taco/tacoservice/ProductController.kt
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ProductController(private val resourceLoader: ResourceLoader) {
 
-    @GetMapping("/tacos", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    @GetMapping("/tacos", produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getTacos(): String {
         return getStringResource("classpath:/tacos.json")
     }
 
-    @GetMapping("/drinks", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    @GetMapping("/drinks", produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getDrinks(): String {
         return getStringResource("classpath:/drinks.json")
     }
 
-    @GetMapping("/salsas", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    @GetMapping("/salsas", produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getSalsas(): String {
         return getStringResource("classpath:/salsas.json")
     }
 
-    @GetMapping("/facts", produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    @GetMapping("/facts", produces = [(MediaType.APPLICATION_JSON_VALUE)])
     fun getFacts(): String {
         return getStringResource("classpath:/facts.json")
     }
